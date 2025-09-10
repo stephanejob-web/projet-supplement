7ad8ab7ac5ccbe7c08249ee98ba776a06ca22996
DARKSH3LL <stephane.job@laplateforme.io>
2025-09-10 10:59:53 +0200

    Refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import './App.css'
-import Header from "./components/Header.tsx";
+import "./App.css";
 import Cards from "./components/Cards.tsx";
 
 function App() {
-
-    return (
-        <div>
-            <Cards
-                title="Gratuit et open source"
-                title1="compatibilite universelle"
-                title2="multiplateforme"
-                title3="fonctionnalités avancées"
-                color="bg-amber-500"
-                title4="ler et performant"
-                title5="Personnalisable"
-                title6="extensible"
-                url="https://cdn.lesnumeriques.com/optim/tutorial/23/237366/86982211-comment-convertir-une-video-youtube-en-mp3-avec-vlc__w1280.jpg"
-            />
-        </div>
-    )
+  return (
+    <div>
+      <Cards
+        leftItems={[
+          "Gratuit et open source",
+          "Compatibilité universelle",
+          "Multiplateforme",
+        ]}
+        rightItems={[
+          "Fonctionnalités avancées",
+          "Léger et performant",
+          "Personnalisable",
+          "Extensible",
+        ]}
+        color="bg-amber-500"
+        url="https://cdn.lesnumeriques.com/optim/tutorial/23/237366/86982211-comment-convertir-une-video-youtube-en-mp3-avec-vlc__w1280.jpg"
+        alt="Illustration VLC"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
